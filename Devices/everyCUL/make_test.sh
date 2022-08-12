ffa1a7cdd29a8f18b54da41fb982206c9c309f0f
Decks <k.kubiak@deckslabs.net>
2022-08-12 17:27:51 +0200

    remove test files used for development

--- Devices/everyCUL/make_test.sh
+++
@@ -1,7 +0,0 @@
-avr-gcc -c -mmcu=atmega4809 -I. -gstabs -Os -Wall -Wstrict-prototypes -std=gnu99  test.c -o test.o
-avr-gcc -mmcu=atmega4809 -I. -gstabs -Os -Wall -Wstrict-prototypes -std=gnu99 test.o --output test.elf -lm
-avr-objcopy -O ihex -R .eeprom -R .fuse test.elf nanoCUL.hex
-avr-objcopy -O binary -j .fuse test.elf nanoCUL.fuse.bin
-
-
-
